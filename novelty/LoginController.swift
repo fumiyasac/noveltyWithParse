ef80044a7eb222e48e0ca6279d94163c4c6e86e3
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2015-09-19 14:40:35 +0900

    Revert "Swift2対応（コンバーター変換）" ※Swift1.2のバージョン
    This reverts commit e96130977d0a44148f5bcce7a437f6d579fc498b.

--- novelty/LoginController.swift
+++ novelty/LoginController.swift
@@ -34,7 +34,7 @@ class LoginController: UIViewController, PFLogInViewControllerDelegate, PFSignUp
         if segue.identifier == "Synopsis"{
             
             //遷移先のコントローラーの変数を用意する
-            let noveltyController = segue.destinationViewController as! NoveltyController
+            var noveltyController = segue.destinationViewController as! NoveltyController
             
             //遷移先のコントローラーに渡したい変数を格納（型を合わせてね）
             noveltyController.novelData = sender
@@ -78,11 +78,18 @@ class LoginController: UIViewController, PFLogInViewControllerDelegate, PFSignUp
         //LogInViewControllerをカスタマイズする（ParseUI.frameworkで提供されているもの）
         let login = PFLogInViewController()
         login.delegate = self
-        login.fields = ([PFLogInFields.UsernameAndPassword, PFLogInFields.LogInButton, PFLogInFields.SignUpButton, PFLogInFields.PasswordForgotten, PFLogInFields.DismissButton])
+        login.fields = (PFLogInFields.UsernameAndPassword
+            | PFLogInFields.LogInButton
+            | PFLogInFields.SignUpButton
+            | PFLogInFields.PasswordForgotten
+            | PFLogInFields.DismissButton)
         
         //SignUpViewControllerをカスタマイズする（ParseUI.frameworkで提供されているもの）
         let signup = PFSignUpViewController()
-        signup.fields = ([PFSignUpFields.UsernameAndPassword, PFSignUpFields.SignUpButton, PFSignUpFields.Email, PFSignUpFields.DismissButton])
+        signup.fields = (PFSignUpFields.UsernameAndPassword
+            | PFSignUpFields.SignUpButton
+            | PFSignUpFields.Email
+            | PFSignUpFields.DismissButton)
         signup.delegate = self
         login.signUpController = signup
         
@@ -122,11 +129,11 @@ class LoginController: UIViewController, PFLogInViewControllerDelegate, PFSignUp
     }
     
     func logInViewController(logInController: PFLogInViewController, didFailToLogInWithError error: NSError?) {
-        print("Failed to log in...")
+        println("Failed to log in...")
     }
     
     func logInViewControllerDidCancelLogIn(logInController: PFLogInViewController) {
-        print("User dismissed the logInViewController")
+        println("User dismissed the logInViewController")
     }
     
     //PFSignUpViewControllerDelegateのメソッド
@@ -135,7 +142,7 @@ class LoginController: UIViewController, PFLogInViewControllerDelegate, PFSignUp
             
         //パスワードは8文字以上でお願いします
         if let password = info["password"] as? String {
-            return password.utf16.count >= 8
+            return count(password.utf16) >= 8
         } else {
             return false
         }
@@ -150,7 +157,7 @@ class LoginController: UIViewController, PFLogInViewControllerDelegate, PFSignUp
     }
     
     func signUpViewController(signUpController: PFSignUpViewController, didFailToSignUpWithError error: NSError?) {
-        print("Failed to sign up...")
+        println("Failed to sign up...")
     }
     
     override func didReceiveMemoryWarning() {
--- novelty/SelectController.swift
+++ novelty/SelectController.swift
@@ -44,7 +44,7 @@ class SelectController: UIViewController, UITableViewDelegate, UITableViewDataSo
         self.novelTable.dataSource = self
         
         //Xibのクラスを読み込む宣言を行う
-        let nib:UINib = UINib(nibName: "novelListCell", bundle: nil)
+        var nib:UINib = UINib(nibName: "novelListCell", bundle: nil)
         self.novelTable.registerNib(nib, forCellReuseIdentifier: "novelListCell")
         
         //Parseからのデータを取得してテーブルに表示する
@@ -81,7 +81,7 @@ class SelectController: UIViewController, UITableViewDelegate, UITableViewDataSo
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         //Xibファイルを元にデータを作成する
-        let cell = tableView.dequeueReusableCellWithIdentifier("novelListCell") as? novelListCell
+        var cell = tableView.dequeueReusableCellWithIdentifier("novelListCell") as? novelListCell
         let novel : AnyObject = novelData.objectAtIndex(indexPath.row)
         
         //各値をセルに入れる
@@ -112,7 +112,7 @@ class SelectController: UIViewController, UITableViewDelegate, UITableViewDataSo
         if segue.identifier == "Login"{
             
             //遷移先のコントローラーの変数を用意する
-            let loginController = segue.destinationViewController as! LoginController
+            var loginController = segue.destinationViewController as! LoginController
             
             //遷移先のコントローラーに渡したい変数を格納（型を合わせてね）
             loginController.novelData = sender
@@ -131,7 +131,7 @@ class SelectController: UIViewController, UITableViewDelegate, UITableViewDataSo
         novelData.removeAllObjects()
         
         //parse.comのデータベースからデータを取得する
-        let query:PFQuery = PFQuery(className: "Novel")
+        var query:PFQuery = PFQuery(className: "Novel")
         //whereKeyメソッドで検索条件を指定
         query.whereKey("category", containsString: "小説")
         //orderByAscendingでカラムに対して昇順で並べる指定
